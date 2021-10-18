e9b65254a5a1a3cc788b93a7959accb8ac34269f
evhenious <evhenious@gmail.com>
2021-10-18 20:49:22 +0300

    Tweaked style - input radius

--- src/components/CitySearch.tsx
+++ src/components/CitySearch.tsx
@@ -5,8 +5,8 @@ import { City } from '../types';
 import CityList from './CityList';
 
 interface Props {
-  currentCity: City | undefined,
-  setCurrentCity: (city: City) => void
+  currentCity: City | undefined;
+  setCurrentCity: (city: City) => void;
 }
 
 const CitySearch: React.FC<Props> = ({ currentCity, setCurrentCity }) => {
@@ -21,13 +21,13 @@ const CitySearch: React.FC<Props> = ({ currentCity, setCurrentCity }) => {
       setCityName(cityList[index].name);
     }
     setCityList([]);
-  }
+  };
 
   const cancelCityChange = () => {
     setCityChosen(true);
     setCityList([]);
     setCityName(currentCity?.name || '');
-  }
+  };
 
   const inputHandler = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
     setCityName(target.value);
@@ -57,27 +57,28 @@ const CitySearch: React.FC<Props> = ({ currentCity, setCurrentCity }) => {
     debouncedSearchHandler(cityName);
   }, [cityName, debouncedSearchHandler]);
 
+  const radiusInput = !!cityList?.length ? 'rounded-tr-md' : 'rounded-r-md';
+  const radiusLabel = !!cityList?.length ? 'rounded-tl-md' : 'rounded-l-md';
+
   return (
     <div className='shadow sm:rounded-md sm:overflow-hidden bg-opacity-60'>
       <div className='px-4 py-5 bg-white space-y-6 sm:p-6'>
-        <div className='grid grid-cols-3 gap-6'>
-          <div className='col-span-3 sm:col-span-2'>
-            <div className='mt-1 flex rounded-md shadow-sm'>
-              <span className='inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm'>
-                Your city is:
-              </span>
-              <input
-                type='text'
-                name='location'
-                id='location'
-                className='focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300 placeholder-gray-400 focus:placeholder-transparent'
-                placeholder='type something, like London'
-                value={cityName}
-                onChange={inputHandler}
-              />
-            </div>
-            {cityList?.length ? <CityList data={cityList} onSelect={selectCity} onCancel={cancelCityChange}/> : false}
+        <div className='block'>
+          <div className='mt-1 flex shadow-sm'>
+            <span className={`inline-flex items-center px-3 ${radiusLabel} border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm`}>
+              Your city is:
+            </span>
+            <input
+              type='text'
+              name='location'
+              id='location'
+              className={`focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none ${radiusInput} sm:text-sm border-gray-300 placeholder-gray-400 focus:placeholder-transparent`}
+              placeholder='type something, like London'
+              value={cityName}
+              onChange={inputHandler}
+            />
           </div>
+          {cityList?.length ? <CityList data={cityList} onSelect={selectCity} onCancel={cancelCityChange} /> : false}
         </div>
       </div>
     </div>
