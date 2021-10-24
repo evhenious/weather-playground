e99b3e62ea31c03560b3fd612e36534bd6a2dfc1
evhenious <evhenious@gmail.com>
2021-10-24 19:24:37 +0300

    Service worker to cache api reqs

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +16,6 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// Registering craft service worker to cache weather data responses
+serviceWorkerRegistration.register();
