d78d475ad9af6dbee60d4696de6fbebdd736e2fc
evhenious <evhenious@gmail.com>
2022-09-30 16:56:28 +0300

    tweaked icons

--- src/components/WeatherTab.tsx
+++ src/components/WeatherTab.tsx
@@ -3,16 +3,16 @@ import { getIcon } from '../utils/iconMapper';
 
 interface Props {
   label?: string;
-  icon?: string;
+  icon?: { name: string, iconClass?: string };
   children?: React.ReactElement[];
 }
 
 const WeatherTab: React.FC<Props> = ({ label, icon, children }) => {
-  const Icon = icon && getIcon(icon);
+  const Icon = icon && getIcon(icon.name);
 
   return (
     <div className='shadow bg-gray-200 bg-opacity-50 rounded-md px-3 py-3 relative'>
-      {Icon && <Icon className='absolute right-3 my-auto h-full w-1/6 top-0' />}
+      {Icon && <Icon className={`absolute right-3 my-auto h-full w-1/6 top-0 ${icon.iconClass || ''}`} />}
       {label && <h2 className='font-semibold'>{label}</h2>}
       {children}
     </div>
